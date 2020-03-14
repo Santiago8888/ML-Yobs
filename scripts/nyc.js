44c5c98cc9330c0d7419e1a13aa73747854a3277
Santiago M <smquintero99@gmail.com>
2020-03-14 11:47:00 -0700

    First succesful location request.

--- scripts/nyc.js
+++ scripts/nyc.js
@@ -5,19 +5,19 @@
     3. Add Salary. (Done)
     4. Parse Description. (Done)
     5. Handle Coordinates. ()
-    6. Handle Category.
-    7. Save First Documents.
-    8. Test Locally.
+    6. Handle Category. ()
+    7. Save First Documents (10?)
+    8. Test Locally. ()
     9. Save Next Batch (1 K).
-    10. Update App.
-    11. Deploy.
-    12. Save Remaning.
+    10. Update App. ()
+    11. Deploy. ()
+    12. Save Remaning. ()
 
 */
 
 
-import { data } from './data/nyc.json'
-
+import { data } from '../data/nyc.json'
+import axios from 'axios'
 
 
 const print = x => console.log(x)
@@ -31,89 +31,61 @@ const capitalize = s => s.split(' ').reduce((d, i, idx, l) =>
 const yob_descriptor = x => x.replace(/â|¢\t|o\t/gi, ' ').replace(/ [^\w]{1,2}s /g, '’s ').split('.')
     .reduce((d, i) => d = d[0]+i.length < 500 ? [d[0] + i.length, d[1] + i + '.'] : d, [0, ''])[1].replace(/\s+/g,' ')
 
-const yob_requirements = x => x.replace(/â.¢\t+/gi, '.').replace(/o\t/gi, '').split('.').filter(x=>x.length>1)
-    .reduce((d, i) => d = d[0]+i.length < 500 ? [d[0] + i.length, [...d[1], `${i.substring(0,i.length-1)}.`.replace(/\s+/g,' ')]] : d, [0, []])[1]
-
-
-    
-
+const yob_requirements = x => x 
+    ? 
+        x.replace(/â.¢\t+/gi, '.').replace(/o\t/gi, '').split('.').filter(x=>x.length>1)
+        .reduce((d, i) => d = d[0]+i.length < 500 ? [d[0] + i.length, [...d[1], `${i.substring(0,i.length-1)}.`.replace(/\s+/g,' ')]] : d, [0, []])[1]
+    :   []
 
 const new_yob = yob => ({
-    doc: () => ({
-        website: null,
-        company: `${capitalize(yob[9])} (${capitalize(yob[20])})`,
-        title: yob[12],
-        salary: `$${add_comma_separator(yob[16])} - $${add_comma_separator(yob[17])}`,
-        address: yob[19],
-        description: yob_descriptor(yob[21]),
-        requirements: yob_requirements(yob[23]),
-        link: yob[25],
+    website: null,
+    company: `${capitalize(yob[9])} (${capitalize(yob[20])})`,
+    title: yob[12],
+    salary: `$${add_comma_separator(yob[16])} - $${add_comma_separator(yob[17])}`,
+    address: yob[19],
+    description: yob_descriptor(yob[21]),
+    requirements: yob_requirements(yob[23]),
+    link: yob[25],
 
-        value: Math.round((Number(yob[16]) + Number(yob[17]))/2)
-    }),
-
-    print(){ print(this.doc()) }
-})
-
-new_yob(data[0]).print()
-
-
-
-
-/*
-
-        "website": null,
-        "company_1": 9,
-        "title": 12,
-        "salary_low": 16,
-        "salary_high": 17,
-        "address": 19,
-        "company_2": 20,
-        "description": 21,
-        "requirements": 23,
-        "link": 25,
-
-*/
-
-
-/*
-const parse_description = description => console.log(description.split('\n').filter(i=>i))
-const parse_requirements = description => {}//console.log('--------------------------------------------     --------------------------------------------')
-const parse_salary = salary => {}
-
-const retrieve_website = company => {}
-const retrieve_coordinates = location => {}
-
-const get_category = title => {}
-
-
-const yob_model = ({
-    name, 
-    slug, 
-    highConcept,
-    yob:{ title, compensation, locationNames, description }
-}) => ({
-    title: title,
-    salary: compensation,
-    address: locationNames[0],
-    company: name,
-    website: retrieve_website(name),
-    pitch: highConcept,
-    description: parse_description(description),
-    link: `https://angel.co/company/${slug}/jobs`,
-    requirements: parse_requirements(description),
-
-    category: get_category(),
-    value: parse_salary(compensation),
-    centroid: retrieve_coordinates(locationNames[0]),
-    city: locationNames[0],
-    source: 'https://angel.co/',
-    hasSalary: parse_salary(compensation) > 0
+    value: Math.round((Number(yob[16]) + Number(yob[17]))/2)
 })
 
 
-const { data: { talent: { jobSearchResults: { startups: { edges }}}}} = angel_json[0] 
-const yobs = edges.map(({ node: { highlightedJobListings, ...edge }}) => yob_model({yob: highlightedJobListings[0], ...edge}))
-*/
+const get_all_locations = () => [...new Set(data.map((yob, i) => `${new_yob(yob).address}, New York, USA`))]
+const locations = get_all_locations()
+const location_token = '116f5028fcc19b'
+const location_settings = address => ({
+    async: true,
+    crossDomain: true,
+    url: encodeURI(`https://us1.locationiq.com/v1/search.php?key=${location_token}&q=${address}&format=json`),
+    method: 'GET'
+})
 
-//print(data[0])
+
+const get_location_url = ({ address }) => `https://us1.locationiq.com/v1/search.php?key=${location_token}&q=${address}&format=json`
+const get_coordinates = async(url) => {
+    const { data = [{}] } = await axios.get(location_url)
+    print(data)
+    print(data[0])
+    print()
+    print()
+    return [data[0].lat, data[0].lon]
+}
+
+const yob_with_location = yob => ({
+    ...new_yob(yob),
+    centroid: get_coordinates(yob)
+})
+
+
+get_coordinates(new_yob(data[0]))
+
+/*
+
+Make request.
+Retrieve data (lat, lng)
+Create new Collection
+Store on new New Collection.
+Retrive coordinates
+
+*/

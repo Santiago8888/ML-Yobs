5493a6e0786e25a476b6194e368cd9bfa491421d
Santiago M <smquintero99@gmail.com>
2020-03-30 20:06:54 -0700

    App Layout Sketeched.

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const NavBar = () => <nav className="navbar" role="navigation" aria-label="main 
 </nav>
 
 
-const Header = () => <div class="notification is-primary">
+const NotificationBar = () => <div className="notification is-primary">
     Software Engineer Jobs RECOMMENDATION ENGINE.
 </div>
 
@@ -62,50 +62,82 @@ const MetricsCard = () => <div className="card">
             <p className={`title is-2 has-text-centered`}> { content } </p> 
         </div>
     </div>
-    <footer class="card-footer">
-        <a href="#" class="card-footer-item">
+    <footer className="card-footer">
+        <a href="#" className="card-footer-item">
             0 <br/> Liked
         </a>
-        <a href="#" class="card-footer-item">
+        <a href="#" className="card-footer-item">
             0 <br/> Rejected
         </a>
     </footer>
 </div>
 
 
-const TechStack = () => <aside class="menu">
-    <p class="menu-label"> Tech Stack </p>
-    <ul class="menu-list">
-        <li><a> Python </a><progress class="progress is-primary" value="15" max="100">15%</progress></li>
-        <li><a> ReactJS </a><progress class="progress is-primary" value="15" max="100">10%</progress></li>
+const TechStack = () => <aside className="menu">
+    <p className="menu-label"> Tech Stack </p>
+    <ul className="menu-list">
+        <li><a> Python </a><progress className="progress is-primary" value="15" max="100">15%</progress></li>
+        <li><a> ReactJS </a><progress className="progress is-primary" value="15" max="100">10%</progress></li>
     </ul>
 </aside>
 
 
-const ChartCard = ({ chart }) => <article class="message">
-    <div class="message-header">
-        <p>Prefered Salary Distribution</p>
-    </div>
-    <div class="message-body"> { chart } </div>
+const ChartCard = ({ title, chart }) => <article className="message">
+    <div className="message-header"><p>{ title }</p></div>
+    <div className="message-body"> { chart } </div>
 </article>
 
 // Salary Chart: https://uber.github.io/react-vis/documentation/series-reference/line-series
 // Industry Chart: https://uber.github.io/react-vis/examples/showcases/radar-charts
 
 
-const YobCard = ({ company, job }) => <div class="card">
-    <header class="card-header">
-        <p class="card-header-title"> { company.name } </p>
+const Contact = () => <div className="container has-text-centered">
+    <nav className="level is-mobile">
+        <div className="level-left">
+            <a className="level-item" aria-label="facebook">
+                <span className="icon is-small">
+                    <i className="fas fa-reply" aria-hidden="true"></i>
+                </span>
+            </a>
+            <a className="level-item" aria-label="tweeter">
+                <span className="icon is-small">
+                    <i className="fas fa-retweet" aria-hidden="true"></i>
+                </span>
+            </a>
+            <a className="level-item" aria-label="email">
+                <span className="icon is-small">
+                    <i className="fas fa-heart" aria-hidden="true"></i>
+                </span>
+            </a>
+            <a className="level-item" aria-label="instagram">
+                <span className="icon is-small">
+                    <i className="fas fa-reply" aria-hidden="true"></i>
+                </span>
+            </a>
+            <a className="level-item" aria-label="slack">
+                <span className="icon is-small">
+                    <i className="fas fa-reply" aria-hidden="true"></i>
+                </span>
+            </a>
+        </div>
+    </nav>
+    <h2 className="subtitle is-5">Follow Us</h2> 
+</div>
+
+
+const YobCard = ({ company, job }) => <div className="card">
+    <header className="card-header">
+        <p className="card-header-title"> { company.name } </p>
     </header>
-    <div class="card-content">
-        <div class="content">
-            <div class="media">
-                <div class="media-left">
-                    <figure class="image is-48x48">
+    <div className="card-content">
+        <div className="content">
+            <div className="media">
+                <div className="media-left">
+                    <figure className="image is-48x48">
                         <img src={company.source} alt={`${company.name}-logo`} />
                     </figure>
                 </div>
-                <div class="media-content">
+                <div className="media-content">
                     <p> {company.description} </p>
                 </div>
                 { /* TODO: Implement Tech Stack Carousel. */} 
@@ -123,90 +155,120 @@ const YobCard = ({ company, job }) => <div class="card">
 			</p>
         </div>
     </div>
-    <footer class="card-footer">
-        <a href="#" class="card-footer-item">Reject</a>
-        <a href="#" class="card-footer-item">Apply</a>
-        <a href="#" class="card-footer-item">Like</a>
+    <footer className="card-footer">
+        <a href="#" className="card-footer-item">Reject</a>
+        <a href="#" className="card-footer-item">Apply</a>
+        <a href="#" className="card-footer-item">Like</a>
     </footer>
 </div>
 
 
-const Main = ({ children }) => <div class="container">
+const Main = ({ children }) => <div className="container">
     { children }
+    <div class="tabs">
+        <ul>
+            <li class="is-active"><a>Suggestions</a></li>
+            <li><a>Map</a></li>
+            <li><a>Kanban</a></li>
+            <li><a>Graph</a></li>
+        </ul>
+    </div>
 </div>
 
 
-const KanbanCard = ({ job }) => <div class="box">
-    <article class="media">
-        <div class="media-left">
-            <figure class="image is-64x64">
+const KanbanCard = ({ job }) => <div className="box">
+    <article className="media">
+        <div className="media-left">
+            <figure className="image is-64x64">
                 <img src={job.logo} alt="Image" />
             </figure>
             { job.company }
         </div>
-    <div class="media-content">
-        <div class="content">
-            <nav class="level is-mobile">
-                <div class="level-left">
-                    <h2 className="title is-4">  { job.title } </h2>
-                </div>
-                <div class="level-right">
-                    <div class="dropdown is-hoverable">
-                        <div class="dropdown-trigger">
-                            <p> { job.stage } </p>
-                            <span class="icon is-small">
-                                <i class="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                        </div>
-                        <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                            <div class="dropdown-content">
-                                <a href="#" class="dropdown-item"> Interested </a>
-                                <a href="#" class="dropdown-item"> Applied </a>
-                                <a href="#" class="dropdown-item"> First Interview </a>
-                                <a href="#" class="dropdown-item"> Technical Stage </a>
-                                <a href="#" class="dropdown-item"> Culture Assesment </a>
-                                <a href="#" class="dropdown-item"> Presented Offer </a>
-                                <a href="#" class="dropdown-item"> Negotiation </a>
-                                <hr class="dropdown-divider" />
-                                <a href="#" class="dropdown-item"> Accepted Offer </a>
-                                <a href="#" class="dropdown-item"> No Longer Interested </a>
-                                <a href="#" class="dropdown-item"> Rejected </a>
+        <div className="media-content">
+            <div className="content">
+                <nav className="level is-mobile">
+                    <div className="level-left">
+                        <h2 className="title is-4">  { job.title } </h2>
+                    </div>
+                    <div className="level-right">
+                        <div className="dropdown is-hoverable">
+                            <div className="dropdown-trigger">
+                                <p> { job.stage } </p>
+                                <span className="icon is-small">
+                                    <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+                                <div className="dropdown-content">
+                                    <a href="#" className="dropdown-item"> Interested </a>
+                                    <a href="#" className="dropdown-item"> Applied </a>
+                                    <a href="#" className="dropdown-item"> First Interview </a>
+                                    <a href="#" className="dropdown-item"> Technical Stage </a>
+                                    <a href="#" className="dropdown-item"> Culture Assesment </a>
+                                    <a href="#" className="dropdown-item"> Presented Offer </a>
+                                    <a href="#" className="dropdown-item"> Negotiation </a>
+                                    <hr className="dropdown-divider" />
+                                    <a href="#" className="dropdown-item"> Accepted Offer </a>
+                                    <a href="#" className="dropdown-item"> No Longer Interested </a>
+                                    <a href="#" className="dropdown-item"> Rejected </a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </nav>
+            </div>
+
+            <nav className="level is-mobile">
+                <div className="level-left">
+                    <a className="level-item" aria-label="Applauses">
+                        <span className="icon is-small"><i className="fas fa-reply" aria-hidden="true"></i></span>
+                    </a>
+                    <a className="level-item" aria-label="Comments">
+                        <span className="icon is-small"><i className="fas fa-retweet" aria-hidden="true"></i></span>
+                    </a>
+                    <a className="level-item" aria-label="Plus">
+                        <span className="icon is-small"><i className="fas fa-heart" aria-hidden="true"></i></span>
+                    </a>
+                    <a className="level-item" aria-label="Minus">
+                        <span className="icon is-small"><i className="fas fa-heart" aria-hidden="true"></i></span>
+                    </a>
+                    <a className="level-item" aria-label="Notes">
+                        <span className="icon is-small"><i className="fas fa-heart" aria-hidden="true"></i></span>
+                    </a>
+                    <a className="level-item" aria-label="To Do">
+                        <span className="icon is-small"><i className="fas fa-heart" aria-hidden="true"></i></span>
+                    </a>
                 </div>
             </nav>
         </div>
-
-        <nav class="level is-mobile">
-            <div class="level-left">
-                <a class="level-item" aria-label="Applauses">
-                    <span class="icon is-small"><i class="fas fa-reply" aria-hidden="true"></i></span>
-                </a>
-                <a class="level-item" aria-label="Comments">
-                    <span class="icon is-small"><i class="fas fa-retweet" aria-hidden="true"></i></span>
-                </a>
-                <a class="level-item" aria-label="Plus">
-                    <span class="icon is-small"><i class="fas fa-heart" aria-hidden="true"></i></span>
-                </a>
-                <a class="level-item" aria-label="Minus">
-                    <span class="icon is-small"><i class="fas fa-heart" aria-hidden="true"></i></span>
-                </a>
-                <a class="level-item" aria-label="Notes">
-                    <span class="icon is-small"><i class="fas fa-heart" aria-hidden="true"></i></span>
-                </a>
-                <a class="level-item" aria-label="To Do">
-                    <span class="icon is-small"><i class="fas fa-heart" aria-hidden="true"></i></span>
-                </a>
-            </div>
-        </nav>
-    </div>
     </article>
 </div>
 
 
-const App = () => ({})
 
+const Layout = () => <div className="columns">
+    <div className="column is-2">
+        <MetricsCard/>
+        <TechStack/>
+    </div>
+    <div className="column is-7">
+        <Main/>
+        <Contact/>
+    </div>
+    <div className="column is-3">
+        <ChartCard title={'By Industry'} chart={<div/>}/>
+        <ChartCard title={'By Location'} chart={<div/>}/>
+        <ChartCard title={'By Salary'} chart={<div/>}/>        
+    </div>
+</div>
+
+
+
+const App = () => <section class="section">
+    <NavBar/>
+    <NotificationBar/>
+    <Layout/>
+</section>
 
 
 export default App
